3cba150ebc60e3310f8424f273f749cd5a7c2df5
spingtseng <80798853+spingtseng@users.noreply.github.com>
2021-05-31 15:23:37 +0800

    Add files via upload

---
+++ app.js
@@ -0,0 +1,8 @@
+function toggleMenu() {
+  var menuShow = document.getElementById("menuShow");
+  if (menuShow.style.display == "block") {
+    menuShow.style.display = "none";
+  } else {
+    menuShow.style.display = "block";
+  }
+}
